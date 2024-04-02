d8cb1fa656c827fcad18f3af460a65a0ae50dafc
PlatonKynin <kininplaton@gmail.com>
2024-04-02 19:48:28 +0300

    01sprint-1week-3task-Button-onClickCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import React, {useState} from 'react';
-import {NewComponent} from "./NewComponent";
+import React from 'react';
+import {MouseEvent} from 'react';
 
 function App() {
-    const [students, setStudents] = useState([
-            {id: 1, name: "James", age: 8},
-            {id: 2, name: "Robert", age: 18},
-            {id: 3, name: "John", age: 28},
-            {id: 4, name: "Michael", age: 38},
-            {id: 5, name: "William", age: 48},
-            {id: 6, name: "David", age: 58},
-            {id: 7, name: "Richard", age: 68},
-            {id: 8, name: "Joseph", age: 78},
-            {id: 9, name: "Thomas", age: 88},
-            {id: 10, name: "Charles", age: 98},
-            {id: 11, name: "Christopher", age: 100},
-            {id: 1, name: "James", age: 8},
-            {id: 2, name: "Robert", age: 18},
-            {id: 3, name: "John", age: 28},
-            {id: 4, name: "Michael", age: 38},
-            {id: 5, name: "William", age: 48},
-            {id: 6, name: "David", age: 58},
-            {id: 7, name: "Richard", age: 68},
-            {id: 8, name: "Joseph", age: 78},
-            {id: 9, name: "Thomas", age: 88},
-            {id: 10, name: "Charles", age: 98},
-            {id: 11, name: "Christopher", age: 100},
-        ]
-    )
+
+    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello im Vasya!')
+    // }
+    //
+    // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello im Ivan!')
+    // }
+
+    const onClickHandler = (name: string) => {
+        console.log(name)
+    }
+
+
     return (
-        <NewComponent students={students}/>
+        <div className={'App'}>
+            <button onClick={(event) => onClickHandler('Vasya!')}>MyYouTubeChanel-1</button>
+            <button onClick={(event) => onClickHandler('Ivan!')}>MyYouTubeChanel-2</button>
+        </div>
     );
 }
 
